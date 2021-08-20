838711d4f666d2ca49ddaf9a3e5f3289a6cc1623
muntasirshihab <86891861+muntasirshihab@users.noreply.github.com>
2021-08-20 20:22:44 +0600

    zzz

---
+++ template.js
@@ -0,0 +1,9 @@
+const firstName = "Justine";
+const lastName = "Timberlake"
+const fullname =`${firstName} ${lastName} is a good boy`;
+
+const multiLine = `hey bro
+Hi shihab
+Hi lubu`;
+
+console.log(multiLine);
